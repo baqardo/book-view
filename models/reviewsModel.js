54df03931a71ee519a9fc356c296edb15c81e4ea
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-07 11:50:14 +0200

    Reviews Model

---
+++ models/reviewsModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    required: [true, "Please provide review"],
+  },
+  rating: {
+    type: Number,
+    required: [true, "Please provide rating"],
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  book: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Book",
+    required: [true, "Review must belong to book"],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "Review must belong to a user"],
+  },
+});
+
+const Review = new mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
