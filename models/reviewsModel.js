8c885d36492112d2c4d542468141a7e40d226234
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-09 11:59:37 +0200

    Reviews model

--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
   review: {
     type: String,
-    required: [true, "Please provide review"],
+    required: [true, 'Please provide review'],
   },
   rating: {
     type: Number,
-    required: [true, "Please provide rating"],
+    required: [true, 'Please provide rating'],
     min: 1,
     max: 5,
   },
@@ -16,17 +16,21 @@ const reviewSchema = new mongoose.Schema({
     default: Date.now(),
   },
   book: {
-    type: mongoose.Schema.ObjectId, //* ISBN
-    ref: "Book",
-    required: [true, "Review must belong to book"],
+    type: String, //* ISBN
+    required: [true, 'Review must belong to book'],
   },
+  // book: {
+  //   type: mongoose.Schema.ObjectId, //* ISBN
+  //   ref: "Book",
+  //   required: [true, "Review must belong to book"],
+  // },
   user: {
     type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: [true, "Review must belong to a user"],
+    ref: 'User',
+    required: [true, 'Review must belong to a user'],
   },
 });
 
-const Review = new mongoose.model("Review", reviewSchema);
+const Review = new mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
