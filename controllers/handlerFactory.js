0f8b966d7964dcda6e70ac14f3c2dc0d75bb8a46
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-09 09:32:03 +0200

    Handling CRUD operations

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,55 @@
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.createOne = Model =>
+  catchAsync(async (req, res) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = Model =>
+  catchAsync(async (req, res) => {
+    const doc = await Model.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) return next(new AppError('No document found with that ID', 404));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.deleteOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) return next(new AppError('No document found with that ID', 404));
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
