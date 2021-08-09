c6e4d9fc2bf05a885d44cd0f2853f905ad6b60ef
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-09 18:29:41 +0200

    Adding forget password route and adding middleware to protect and restrict to only certain users paths

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -8,9 +8,17 @@ const {
   deleteReview,
 } = require('../controllers/reviewController');
 
+const { protect, restrictTo } = require('../controllers/authController');
+
 const router = express.Router();
 
-router.route('/').get(getAllReviews).post(createReview);
-router.route('/:id').get(getReview).patch(updateReview).delete(deleteReview);
+router.use(protect);
+
+router.route('/').get(getAllReviews).post(restrictTo('user'), createReview);
+router
+  .route('/:id')
+  .get(getReview)
+  .patch(restrictTo('user', 'admin'), updateReview)
+  .delete(restrictTo('user', 'admin'), deleteReview);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const { getAllUsers, getUser, updateUser, deleteUser, createUser } = require('../controllers/userController');
+const { signup, login, forgotPassword, resetPassword, protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
-// router.post("/signup", signup);
-// router.post("/login", login);
+router.post('/signup', signup);
+router.post('/login', login);
+
+router.post('/forgotPassword', forgotPassword);
+router.post('/resetPassword/:token', resetPassword);
+
+router.use(protect);
+
 // router.get("/logout", logout);
-
-// router.post("/forgotPassword", forgotPassword);
-// router.post("/resetPassword/:token", resetPassword);
-
-// router.use(protect);
-
 // router.patch("/updateMyPassword", updatePassword);
 // router.get("/me", getMe, getUser);
 // router.patch("/updateMe", uploadUserPhoto, resizeUserPhoto, updateMe);
 // router.delete("/deleteMe", deleteMe);
 
 //! Routes only for admin
-// router.use(restrictTo("admin"));
+router.use(restrictTo('admin'));
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
