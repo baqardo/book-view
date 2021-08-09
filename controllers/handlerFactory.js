954ccbbf54073366ac1d7b984afe7777492fc1e3
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-09 10:56:53 +0200

    updateOne handler

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -13,6 +13,23 @@ exports.createOne = Model =>
     });
   });
 
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) return next(new AppError('No document found with that ID', 404));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
 exports.getAll = Model =>
   catchAsync(async (req, res) => {
     const doc = await Model.find();
