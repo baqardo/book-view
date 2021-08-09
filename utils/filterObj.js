90e8438554e3ce492500234eb7e4bbc861453397
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-09 09:29:43 +0200

    Body filter
    - Filter out fields
    - Hold only specified fields

---
+++ utils/filterObj.js
@@ -0,0 +1,15 @@
+exports.removeFields = (obj, ...disallowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (!disallowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+exports.holdFields = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
