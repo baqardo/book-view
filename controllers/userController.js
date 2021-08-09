63688567ecd8d73013d3956b62735af5d8c731f2
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-09 19:26:37 +0200

    Removing possibility of changing password by admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,30 +4,7 @@ const AppError = require('../utils/appError');
 const filterObj = require('../utils/filterObj');
 const factory = require('./handlerFactory');
 
-exports.updateUser = catchAsync(async (req, res, next) => {
-  const filteredBody = filterObj.removeFields(req.body, 'password', 'passwordConfirm');
-
-  const updatedUser = await User.findByIdAndUpdate(req.params.id, filteredBody, {
-    new: true,
-    runValidators: true,
-  });
-
-  if (!updatedUser) return next(new AppError('No document found with that ID', 404));
-
-  if (req.body.password && req.body.passwordConfirm) {
-    updatedUser.password = req.body.password;
-    updatedUser.passwordConfirm = req.body.passwordConfirm;
-    await updatedUser.save();
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data: updatedUser,
-    },
-  });
-});
-
+exports.updateUser = factory.updateOne(User); //! Do not update password with this
 exports.createUser = factory.createOne(User);
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
