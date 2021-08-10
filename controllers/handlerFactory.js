e52cc825fdd03158e431ccdff7d8806097f880e8
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-10 12:49:47 +0200

    Adding API Features
    - filtering by fields
    - sorting by fields
    - limiting to specified fields
    - pagination

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.createOne = Model =>
   catchAsync(async (req, res) => {
@@ -35,7 +36,8 @@ exports.updateOne = Model =>
 
 exports.getAll = Model =>
   catchAsync(async (req, res) => {
-    const doc = await Model.find();
+    const features = new APIFeatures(Model.find(), req.query).filter().sort().limitFields().paginate();
+    const doc = await features.query;
 
     res.status(200).json({
       status: 'success',
