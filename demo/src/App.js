1821becdb4bd42adb2175eeb4805d47388f2fd9f
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-12 13:51:55 +0200

    Creating app structure
    - Header component
    - Routing to Home component
    - Routing to BookDetails component
    - Footer component

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,35 +1,29 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './containers/Home/Home';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import BookDetails from './components/BookDetails/BookDetails';
 
 class App extends Component {
-  state = {
-    data: null,
-  };
-
-  componentDidMount() {
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-  // fetching the GET route from the Express server which matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message);
-    }
-    return body;
-  };
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.data}</p>
-      </div>
+      <Router>
+        <div className="app">
+          <Header />
+          <Switch>
+            <Route path="/book/:id/author/:author">
+              <BookDetails />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
