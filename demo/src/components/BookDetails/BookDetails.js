c3b5c94e527779dc9e2a8579a6dfd4690e4b4c45
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-12 17:29:36 +0200

    Getting data from API's

--- demo/src/components/BookDetails/BookDetails.js
+++ demo/src/components/BookDetails/BookDetails.js
@@ -1,32 +1,110 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import './BookDetails.scss';
-import { useParams } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import axios from 'axios';
 
-const BookDetails = props => {
-  const { id, author } = useParams();
-  useEffect(() => {
-    getBookDetails(id);
-  });
+class BookDetails extends Component {
+  constructor(props) {
+    super(props);
+    const { id, author } = this.props.match.params;
 
-  return <div>Book ID: {id}</div>;
-};
+    this.state = {
+      id,
+      author,
+      loaded: false,
+    };
+  }
 
-const getBookDetails = async id => {
-  const response = await axios.get(`https://openlibrary.org/works/${id}.json`);
-  const data = response.data;
+  componentDidMount() {
+    this.getBookDetails().then(res => {
+      this.setState(state => ({
+        ...state,
+        ...res,
+      }));
+    });
+    this.getBookRatings().then(res => {
+      this.setState(state => ({
+        ...state,
+        ...res,
+      }));
+    });
+  }
 
-  const bookDetails = {
-    cover: `http://covers.openlibrary.org/b/olid/${id}-M.jpg`,
-    title: data.title,
-    firstPublishDate: data.first_publish_date,
-    description: data.description,
-    subjects: data.subjects.slice(0, 10),
-  };
+  async getBookDetails() {
+    const id = this.state.id;
+    const response = await axios.get(`https://openlibrary.org/works/${id}.json`);
+    const data = response.data;
 
-  console.log(bookDetails);
+    const bookDetails = {
+      cover: `http://covers.openlibrary.org/b/olid/${id}-M.jpg`,
+      title: data.title,
+      publishDate: data.first_publish_date,
+      description: data.description,
+      subjects: data.subjects.slice(0, 10),
+      loaded: true,
+    };
 
-  return bookDetails;
-};
+    return bookDetails;
+  }
 
-export default BookDetails;
+  async getBookRatings() {
+    const id = this.state.id;
+    const response = await axios.get(`http://localhost:8080/api/v1/books/OLID/${id}`);
+    const { data } = response.data;
+
+    const bookRatings = {
+      ratingsAverage: data.ratingsAverage,
+      ratingsQuantity: data.ratingsQuantity,
+      reviews: data.reviews,
+    };
+
+    console.log(data);
+
+    return bookRatings;
+  }
+
+  render() {
+    return (
+      this.state.loaded && (
+        <div>
+          ID: {this.state.id}
+          {console.log(this.state)}
+          <br />
+          Author: {this.state.author}
+          <br />
+          Title: {this.state.title}
+          <br />
+          Cover: {this.state.cover}
+          <br />
+          Publish Date: {this.state.publishDate}
+          <br />
+          Description: {this.state.description}
+          <br />
+          Subjects:{' '}
+          <ul>
+            {this.state.subjects.map(subject => (
+              <li key={subject}>{subject}</li>
+            ))}
+          </ul>
+          Average Ratings: {this.state.ratingsAverage}
+          <br />
+          Ratings Quantity: {this.state.ratingsQuantity}
+          <br />
+          Reviews: <br />
+          <ul>
+            {this.state.reviews.map(review => (
+              <li key={review._id}>
+                Review: {review.review}
+                <br /> Rating: {review.rating}
+                <br /> Username: {review.user.name}
+                <br /> User Photo: {review.user.photo}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    );
+  }
+}
+
+export default withRouter(BookDetails);
