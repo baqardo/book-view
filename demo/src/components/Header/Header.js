55c0d97bf339bcd69a0ac297891f35ea160b4089
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-12 13:52:44 +0200

    Header Nav and Footer components

---
+++ demo/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+import './Header.scss';
+
+const Header = props => (
+  <header className="header">
+    <Nav />
+  </header>
+);
+
+export default Header;
---
+++ demo/src/components/Nav/Nav.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = props => <nav className="nav"></nav>;
+
+export default Nav;
