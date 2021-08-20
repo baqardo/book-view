f3cc3d498c9fe80dfff4651ef86367a1347e8fd5
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-08-20 11:47:20 +0200

    Sending cookies with JWT to client

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,16 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const dayInMilliseconds = 24 * 60 * 60 * 1000;
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_EXPIRES_IN * dayInMilliseconds),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   user.password = undefined;
 
   res.status(statusCode).json({
